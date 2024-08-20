de045e519bdbb65195d62f294d9681c992579b07
ahmedfouaddev <ahmedfouaddev@gmail.com>
2024-08-20 20:34:48 +0300

    Init

---
+++ eventkoi.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: EventKoi
+ * Plugin URI: https://eventkoi.com
+ * Description: WordPress event plugin.
+ * Author: EventKoi
+ * Author URI: https://eventkoi.com
+ * Requires at least: 6.6
+ * Requires PHP: 7.4
+ * Version: 1.0.0
+ * Text Domain: eventkoi
+ * Domain Path: /languages/
+ *
+ * @package EventKoi.
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
